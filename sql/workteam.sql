ccfec19b42763549f96a0449572aedb6b0b11f32
chufengpei <chufengpei@qq.com>
2019-06-03 14:52:08 +0800

    初始化

--- sql/workteam.sql
+++ sql/workteam.sql
@@ -1,48 +1,37 @@
-drop table if EXISTS `projects`;
-drop table if EXISTS `teams`;
-drop table if EXISTS `members`;
 drop table if EXISTS `tasks`;
-drop table if EXISTS `users`;
+drop table if EXISTS `members`;
+drop table if EXISTS `teams`;
+drop table if EXISTS `projects`;
+
 CREATE TABLE IF NOT EXISTS `projects`(
-   `id` INT UNSIGNED AUTO_INCREMENT,
-   `project_id` varchar(30) NOT NULL UNIQUE,
-   `cid` varchar(30) NOT NULL,
-   `class_name` VARCHAR(30) NOT NULL default '1610A',
+   `project_id` varchar(50) NOT NULL UNIQUE,
+   `cid` varchar(50) NOT NULL,
+   `class_name` VARCHAR(10) NOT NULL default '1610A',
    `project_name` VARCHAR(100) NOT NULL UNIQUE,
-   `project_discription` VARCHAR(100) DEFAULT '',
-   `create_time` VARCHAR(100) DEFAULT '',
-   PRIMARY KEY ( `id` )
+   `project_description` VARCHAR(100) DEFAULT '',
+   `create_time` DATETIME default LOCALTIMESTAMP(),
+   PRIMARY KEY ( `project_id` ),
+   FOREIGN KEY (`cid`) REFERENCES classes(`cid`),
+   UNIQUE KEY `cpd`(`cid`,`project_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `users`(
-   `id` INT UNSIGNED AUTO_INCREMENT,
-   `user_id` varchar(30) NOT NULL UNIQUE,
-   `identity` tinyint NOT NULL comment '0-管理员,1-teacher,2-student,3',
-   `user_name` VARCHAR(30) NOT NULL '',
-   `user_pwd` VARCHAR(20) NOT NULL,
-   `user_class` VARCHAR(6) DEFAULT '',
-   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   PRIMARY KEY ( `id` )
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 CREATE TABLE IF NOT EXISTS `teams`(
    `id` INT UNSIGNED AUTO_INCREMENT,
    `team_id` VARCHAR(100) NOT NULL UNIQUE,
-   `pid` varchar(30) NOT NULL,
+   `project_id` varchar(50) NOT NULL,
    `team_name` VARCHAR(100) NOT NULL,
    `team_gitadress` VARCHAR(100) DEFAULT '' UNIQUE,
-   `create_time` VARCHAR(100) DEFAULT '',
+   `create_time` DATETIME default LOCALTIMESTAMP(),
    PRIMARY KEY ( `id` ),
-   FOREIGN KEY (`pid`) REFERENCES projects(`project_id`),
-   UNIQUE KEY `pid_tname_git`(`pid`,`team_name`)
+   FOREIGN KEY (`project_id`) REFERENCES projects(`project_id`),
+   UNIQUE KEY `pid_tname`(`project_id`,`team_name`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 0: teacher  1: 组长  2:组员
 
 CREATE TABLE IF NOT EXISTS `members`(
    `id` INT(7) UNSIGNED zerofill AUTO_INCREMENT,
-   `mid` varchar(30) NOT NULL UNIQUE,
+   `member_id` varchar(50) NOT NULL UNIQUE,
    `member_name` VARCHAR(100) NOT NULL,
    `member_gender` tinyint NOT NULL,
    `identity` tinyint NOT NULL,
@@ -59,14 +48,15 @@ CREATE TABLE IF NOT EXISTS `members`(
 CREATE TABLE IF NOT EXISTS `tasks`(
    `tid` INT(8) UNSIGNED zerofill AUTO_INCREMENT,
    `task_name` VARCHAR(100) NOT NULL,
+   `sid` varchar(50) NOT NULL UNIQUE,
    `task_status` tinyint default 0,
    `task_checked` tinyint default 0,
    `check_person` VARCHAR(100) default '',
-   `mid` varchar(30) NOT NULL,
-   `team_id` varchar(30) NOT NULL,
-   `pid` varchar(30) NOT NULL,
+   `member_id` varchar(50) NOT NULL,
+   `team_id` varchar(50) NOT NULL,
+   `project_id` varchar(50) NOT NULL,
    PRIMARY KEY ( `tid` ),
-   FOREIGN KEY (`mid`) REFERENCES members(`mid`),
-   UNIQUE KEY `mid_task_name`(`mid`,`task_name`)
+   FOREIGN KEY (`member_id`) REFERENCES members(`member_id`),
+   UNIQUE KEY `mid_task_name`(`member_id`,`task_name`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
